3975fc67fd8daaee0352fdb5faf2afb2e38673fa
Tahsin Anika <anika.tahsin97@gmail.com>
2020-09-04 23:18:00 +0600

    Solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cacheSolve(makeCacheMatrix()) will take in any nonsingular matrix 
+## and produce value based on previous inputs
 
-## Write a short comment describing this function
+
+## Default value of m set as null so whenever x is reset m is also reset to null
+## The new x is inversed and cached in setimat which is retrieved on next call
+## via getimat
+## Provide a list which can be accessed by cacheSolve using $ operator
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                   m <- NULL
+                   set <- function(y) {
+                      x <<- y
+                      m <<- NULL
+  }
+                   get <- function() x
+                   setimat <- function(solve) m <<- solve
+                   getimat <- function() m
+                   list(set = set, get = get,
+                        setimat = setimat,
+                        getimat = getimat)
 }
 
-
-## Write a short comment describing this function
+## Retrieves getimat from makeCacheMatrix & if m is !null then prints cache value
+## If m is null then inverses input matrix and assign it to setimat for next call
 
 cacheSolve <- function(x, ...) {
+              m <- x$getimat()
+              if(!is.null(m)) {
+                message("getting cached data")
+              return(m)
+  }
+              data <- x$get()
+              m <- solve(data, ...)
+              x$setimat(m)
+              m
         ## Return a matrix that is the inverse of 'x'
 }
